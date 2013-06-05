0fd4ad095095da65564b29e7aadf463600eb636b
Richo Healey <richo@psych0tik.net>
2013-06-05 23:08:28 +1000

    Shuffle stuff where android wants it

--- android/src/net/psych0tik/pebblegps/pebblegpsActivity.java
+++ android/src/net/psych0tik/pebblegps/pebblegpsActivity.java
@@ -6,6 +6,9 @@ import android.location.LocationManager;
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.util.PebbleDictionary;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 import java.util.UUID;
 
 /**
