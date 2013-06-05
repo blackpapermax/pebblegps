7edda290068178b28d6c6ce75adf33a9e46d0005
Richo Healey <richo@psych0tik.net>
2013-06-05 23:24:32 +1000

    Vendor in PebbleKit, updated for my android version

---
+++ android/PebbleKit/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Volumes/Code/code/ext/pebblegps/android/PebbleKit/bin/AndroidManifest.xml : \
+/Volumes/Code/code/ext/pebblegps/android/PebbleKit/AndroidManifest.xml \
---
+++ android/PebbleKit/gen/R.java.d
@@ -0,0 +1,4 @@
+/Volumes/Code/code/ext/pebblegps/android/PebbleKit/gen/com/getpebble/android/kit/R.java \
+ : /Volumes/Code/code/ext/pebblegps/android/PebbleKit/res/layout/main.xml \
+/Volumes/Code/code/ext/pebblegps/android/PebbleKit/res/values/strings.xml \
+/Volumes/Code/code/ext/pebblegps/android/PebbleKit/bin/AndroidManifest.xml \
